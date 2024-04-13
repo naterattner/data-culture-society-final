08498c386ee37342aacbb7699e645f50bd48a91b
naterattner <naterattner@gmail.com>
2024-04-13 17:08:09 -0400

    absolute path for data

--- scripts/main.js
+++ scripts/main.js
@@ -45,7 +45,7 @@ let state = {
 
 // data = './assets/data/merged_data_1yr.json'
 
-fetch('../assets/data/output.json')
+fetch('https://raw.githubusercontent.com/naterattner/data-culture-society-final/master/assets/data/output.json')
     .then(response => response.json())
     .then(data => {
         // Process the JSON data
